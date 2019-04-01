f42dca5ba3fb9001b525d27a355cd623226afeea
charles <arobaz112@hotmail.fr>
2019-04-01 18:23:51 +0200

    node port 3000

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ const currentGame = {
 };
 
 // Start listening
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
