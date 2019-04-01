161d79cfb61c03bbd62d4d51d9f2ab57b0a15a62
charles <arobaz112@hotmail.fr>
2019-04-01 19:54:14 +0200

    dont serve static files

--- src/server.js
+++ src/server.js
@@ -20,10 +20,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+/*app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/dist/index.html'))
-});
+});*/
 
 const currentGame = {
   currentQuestion: {
