f2e39f29ebf6713f9590660141672d634535d12e
charles <arobaz112@hotmail.fr>
2019-04-03 18:12:43 +0200

    --no-save after npm install in prod

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,10 +3,10 @@
 ssh charles@51.77.202.235 <<EOF
  cd /var/www/footballquizz
  git pull
- npm install -- production
+ npm install -- production --no-save
  npm run build
  cd client
- npm install -- production
+ npm install -- production --no-save
  npm run build
  pm2 restart all
  exit
